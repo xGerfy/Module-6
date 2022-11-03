45280e56633ec0ad10a3e00d563820559aa612a7
Roman Ponomarev <roman.ponomarev66@gmail.com>
2022-11-03 12:54:52 +0400

    created remove function

--- index.php
+++ index.php
@@ -6,3 +6,18 @@ function add (&$textStorage, $title, $text) {
     $textStorage[] = ['title' => $title, 'text' => $text];
 }
 
+add($textStorage, 'hello', 'world');
+add($textStorage, 'привет', 'мир');
+var_dump($textStorage);
+
+function remove (&$textStorage, $a) {
+    foreach ($textStorage as $key => $value) {
+        if ($a == $key || $a == $value) {
+            unset($textStorage[$a]);
+        }
+    }
+}
+
+remove($textStorage, 0);
+remove($textStorage, 5);
+var_dump($textStorage);
